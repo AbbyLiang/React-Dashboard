3e0843d7ed1641be080a94261ca4744a9d6383c9
LiangYao <Yao.Liang@desay-svautomotive.com>
2021-04-19 11:57:40 +0800

    my first react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">button</Button>
+      <div>111</div>
+      <p></p>
+    </div>
+  );
+}
+
+
+
+export default App;
